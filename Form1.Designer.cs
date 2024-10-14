18f9ec7fbf78fbd6d3d660e3005c16ed41875509
zadort <127024072+zadort@users.noreply.github.com>
2024-10-14 11:09:15 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace atomreaktor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.beinditas = new System.Windows.Forms.Button();
+            this.leallitas = new System.Windows.Forms.Button();
+            this.generaltEnergiaMennyiseg = new System.Windows.Forms.Button();
+            this.hofok = new System.Windows.Forms.Button();
+            this.hutovizBeengedese = new System.Windows.Forms.Button();
+            this.kiiratasHomerseklet = new System.Windows.Forms.RichTextBox();
+            this.kiiratasGeneraltEnergia = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // beinditas
+            // 
+            this.beinditas.Location = new System.Drawing.Point(12, 109);
+            this.beinditas.Name = "beinditas";
+            this.beinditas.Size = new System.Drawing.Size(75, 23);
+            this.beinditas.TabIndex = 0;
+            this.beinditas.Text = "Beindítás";
+            this.beinditas.UseVisualStyleBackColor = true;
+            this.beinditas.Click += new System.EventHandler(this.beinditas_Click);
+            // 
+            // leallitas
+            // 
+            this.leallitas.Location = new System.Drawing.Point(93, 109);
+            this.leallitas.Name = "leallitas";
+            this.leallitas.Size = new System.Drawing.Size(75, 23);
+            this.leallitas.TabIndex = 1;
+            this.leallitas.Text = "Leállítás";
+            this.leallitas.UseVisualStyleBackColor = true;
+            this.leallitas.Click += new System.EventHandler(this.leallitas_Click);
+            // 
+            // generaltEnergiaMennyiseg
+            // 
+            this.generaltEnergiaMennyiseg.Location = new System.Drawing.Point(174, 109);
+            this.generaltEnergiaMennyiseg.Name = "generaltEnergiaMennyiseg";
+            this.generaltEnergiaMennyiseg.Size = new System.Drawing.Size(75, 23);
+            this.generaltEnergiaMennyiseg.TabIndex = 2;
+            this.generaltEnergiaMennyiseg.Text = "Generált energia mennyiség";
+            this.generaltEnergiaMennyiseg.UseVisualStyleBackColor = true;
+            this.generaltEnergiaMennyiseg.Click += new System.EventHandler(this.generaltEnergiaMennyiseg_Click);
+            // 
+            // hofok
+            // 
+            this.hofok.Location = new System.Drawing.Point(255, 109);
+            this.hofok.Name = "hofok";
+            this.hofok.Size = new System.Drawing.Size(75, 23);
+            this.hofok.TabIndex = 3;
+            this.hofok.Text = "Hőfok";
+            this.hofok.UseVisualStyleBackColor = true;
+            this.hofok.Click += new System.EventHandler(this.hofok_Click);
+            // 
+            // hutovizBeengedese
+            // 
+            this.hutovizBeengedese.Location = new System.Drawing.Point(336, 109);
+            this.hutovizBeengedese.Name = "hutovizBeengedese";
+            this.hutovizBeengedese.Size = new System.Drawing.Size(75, 23);
+            this.hutovizBeengedese.TabIndex = 4;
+            this.hutovizBeengedese.Text = "Hűtővíz beengedése";
+            this.hutovizBeengedese.UseVisualStyleBackColor = true;
+            this.hutovizBeengedese.Click += new System.EventHandler(this.hutovizBeengedese_Click);
+            // 
+            // kiiratasHomerseklet
+            // 
+            this.kiiratasHomerseklet.Location = new System.Drawing.Point(12, 7);
+            this.kiiratasHomerseklet.Name = "kiiratasHomerseklet";
+            this.kiiratasHomerseklet.Size = new System.Drawing.Size(100, 96);
+            this.kiiratasHomerseklet.TabIndex = 5;
+            this.kiiratasHomerseklet.Text = "";
+            this.kiiratasHomerseklet.TextChanged += new System.EventHandler(this.kiiratasHomerseklet_TextChanged_1);
+            // 
+            // kiiratasGeneraltEnergia
+            // 
+            this.kiiratasGeneraltEnergia.Location = new System.Drawing.Point(118, 7);
+            this.kiiratasGeneraltEnergia.Name = "kiiratasGeneraltEnergia";
+            this.kiiratasGeneraltEnergia.Size = new System.Drawing.Size(100, 96);
+            this.kiiratasGeneraltEnergia.TabIndex = 6;
+            this.kiiratasGeneraltEnergia.Text = "";
+            this.kiiratasGeneraltEnergia.TextChanged += new System.EventHandler(this.kiiratasGeneraltEnergia_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kiiratasGeneraltEnergia);
+            this.Controls.Add(this.kiiratasHomerseklet);
+            this.Controls.Add(this.hutovizBeengedese);
+            this.Controls.Add(this.hofok);
+            this.Controls.Add(this.generaltEnergiaMennyiseg);
+            this.Controls.Add(this.leallitas);
+            this.Controls.Add(this.beinditas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button beinditas;
+        private System.Windows.Forms.Button leallitas;
+        private System.Windows.Forms.Button generaltEnergiaMennyiseg;
+        private System.Windows.Forms.Button hofok;
+        private System.Windows.Forms.Button hutovizBeengedese;
+        private System.Windows.Forms.RichTextBox kiiratasHomerseklet;
+        private System.Windows.Forms.RichTextBox kiiratasGeneraltEnergia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace atomreaktor
+{
+    public partial class Form1 : Form
+    {
+        int homerseklet = 0;
+        int energia = 0;
+        bool online;
+        public Form1()
+        {
+            InitializeComponent();
+            kiiratasHomerseklet.Text = homerseklet.ToString();
+            kiiratasHomerseklet.Text = energia.ToString();
+        }
+
+        private void beinditas_Click(object sender, EventArgs e)
+        {
+            online = true;
+            Random r = new Random();
+            homerseklet = r.Next(40, 101);
+            kiiratasHomerseklet.Text = homerseklet.ToString();
+            kiiratasHomerseklet.Text = energia.ToString();
+        }
+
+        private void leallitas_Click(object sender, EventArgs e)
+        {
+            if (homerseklet < 70)
+            {
+                online = false;
+                MessageBox.Show("A reaktor sikeresn leállt!");
+            }
+            else {
+                MessageBox.Show("Túl magas a hőmrérséklet. Kérlek, hűtsd le a reaktort!");
+            }
+        }
+
+        private void generaltEnergiaMennyiseg_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hofok_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hutovizBeengedese_Click(object sender, EventArgs e)
+        {
+            homerseklet = 40;
+            MessageBox.Show("Hűtővíz beengedése...");
+        }
+
+        private void kiiratasHomerseklet_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kiiratasGeneraltEnergia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kiiratasHomerseklet_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
